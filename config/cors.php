35eb4beaed859d0a3dc4a03ce419a30d8ab968c1
mcimperial <mc_cimperial@yahoo.com>
2025-05-29 15:54:18 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://employee-portal.llibi.app'],
 
     'allowed_origins_patterns' => [],
 
