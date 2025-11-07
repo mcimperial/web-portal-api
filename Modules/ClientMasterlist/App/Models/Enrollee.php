d42b9144754cecb8fcc35b71a721f4b43533191b
mcimperial <mc_cimperial@yahoo.com>
2025-11-07 14:43:57 +0800

    Update Enrollee.php

--- Modules/ClientMasterlist/App/Models/Enrollee.php
+++ Modules/ClientMasterlist/App/Models/Enrollee.php
@@ -70,7 +70,7 @@ class Enrollee extends Model
     // Principal has many dependents
     public function dependents()
     {
-        return $this->hasMany(Dependent::class, 'principal_id');
+        return $this->hasMany(Dependent::class, 'principal_id')->whereNull('deleted_at');
     }
 
     // Principal has one insurance provider through enrollment
