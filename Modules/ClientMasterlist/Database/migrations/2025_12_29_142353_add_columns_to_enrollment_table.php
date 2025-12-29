bf0f533ed78d880e0d0862b8f19df4ee75701629
mcimperial <mc_cimperial@yahoo.com>
2025-12-29 16:43:38 +0800

    Add age and premium restriction fields to enrollment
    Introduces new fields 'age_restriction', 'hierarchy_options', and 'premium_restriction' to the enrollment model and database table. Updates controllers to handle these fields in validation, premium computation, and dependent overage logic, allowing for more flexible enrollment and premium rules based on age and other criteria.

---
+++ Modules/ClientMasterlist/Database/migrations/2025_12_29_142353_add_columns_to_enrollment_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cm_enrollment', function (Blueprint $table) {
+            $table->integer('age_restriction')->nullable()->after('with_monthly');
+            $table->string('hierarchy_options')->nullable()->after('age_restriction');
+            $table->string('premium_restriction')->nullable()->after('hierarchy_options');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cm_enrollment', function (Blueprint $table) {
+            $table->dropColumn(['age_restriction', 'hierarchy_options', 'premium_restriction']);
+        });
+    }
+};
